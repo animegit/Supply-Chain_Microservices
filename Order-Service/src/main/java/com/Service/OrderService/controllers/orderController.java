4d1cdbeec4c3dc66b6c181a7e3e88a81d727552d
animegit <ajitanimesh1234@gmail.com>
2024-04-10 19:14:12 +0530

    Implemented Circuit Breaker

--- Order-Service/src/main/java/com/Service/OrderService/controllers/orderController.java
+++ Order-Service/src/main/java/com/Service/OrderService/controllers/orderController.java
@@ -2,21 +2,39 @@ package com.Service.OrderService.controllers;
 
 
 import com.Service.OrderService.dto.OrderRequest;
+import com.Service.OrderService.models.Order;
 import com.Service.OrderService.services.OrderServices;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
 @AllArgsConstructor
+@Slf4j
 public class orderController {
 private OrderServices orderServices;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) throws IllegalAccessException {
-        orderServices.placeorder(orderRequest);
-        return "Order";
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallback")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+  
+    public CompletableFuture<String> placeOrder (@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
+        CompletableFuture<U> uCompletableFuture = CompletableFuture.supplyAsync(() -> orderServices.placeorder(orderRequest));
+        return uCompletableFuture;
+    }
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        log.info("Cannot Place Order Executing Fallback logic");
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
     }
 }
